cfb72dc894f10684259ddb994b9311e64c201a0b
Sergio-21-ops <83419882+Sergio-21-ops@users.noreply.github.com>
2023-06-07 08:25:20 -0300

    rutas,vistas y componentes

--- scripts/router/rutas.js
+++ scripts/router/rutas.js
@@ -1,4 +1,24 @@
+const home = { 
+  template: `<div><h2 class="text-center my-5">Bienvenidos a Prenpub</h2></div>`,
+  name:"componenteHome"
+};
+const publicar = {
+  template: `<componente-pnoticias> </componente-pnoticias>`, 
+  name:"formulario"
+};
+const ver = {
+  template: `<ver-componente></ver-componente>`, 
+  name:"componentevVer"
+};
+const routes = [
+  { path: '/', component: home },
+  { path: '/publicar', component: publicar },
+  { path: '/ver', component: ver }
 
+];
+const router = new VueRouter({
+	  routes
+});
 const app = new Vue({
 	el:"#app",
   data: {
